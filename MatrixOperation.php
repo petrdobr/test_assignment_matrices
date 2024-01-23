d61d76e8d241a6b418a078f13a8fe03b62206c2e
Petr Dobrokhotov <123838453+petrdobr@users.noreply.github.com>
2024-01-23 22:16:50 +0400

    add sum and subtraction

--- MatrixOperation.php
+++ MatrixOperation.php
@@ -13,14 +13,38 @@ class MatrixOperation
         $this->B = $B;
     }
 
-    public function isEmptyMatrix($A)
-    {
-        
-    }
-
     public function haveEqualDimensions(array $A, array $B)
     {
-        //check if dimensions are equal
+        $rowsA = count($A);
+        if (!is_array($A[0])) {
+            $colsA = 1;
+        } else {
+            $colsA = count($A[0] ?? []);
+        }
+        
+        $rowsB = count($B);
+        if (!is_array($B[0])) {
+            $colsB = 1;
+        } else {
+            $colsB = count($A[0] ?? []);
+        }
+    
+        return ($rowsA === $rowsB) && ($colsA === $colsB);
+    }
+
+    public function isMatrix(array $A)
+    {
+        if(!is_array($A[0])) {
+            return count($A) > 0;
+        }
+        $cols = count($A[0] ?? []);
+
+        foreach ($A as $row) {
+            if (count($row) !== $cols) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public function sum(array $A = [], array $B = [])
@@ -28,9 +52,51 @@ class MatrixOperation
         $A = empty($A) ? $this->A : $A;
         $B = empty($B) ? $this->B : $B;
         if (empty($A) || empty($B)) {
-            return 'Error: not specified matrices for the sum operation';
+            return 'Error: not specified matrices for the sum operation.';
         }
-        return 'SUM';
+        if (!$this->isMatrix($A) || !$this->isMatrix($B)) {
+            return 'Error: at least one input matrix has wrong shape';
+        }
+        if (!$this->haveEqualDimensions($A, $B)) {
+            return 'Error: matrices have different dimensions.';
+        }
+        $item = function ($aRow, $bRow) use (&$item) {
+            if (!is_array($aRow) && !is_array($bRow)) {
+                return $aRow + $bRow;
+            }
+    
+            return array_map(function ($a, $b) use ($item) {
+                return $item($a, $b);
+            }, $aRow, $bRow);
+        };
+    
+        return $item($A, $B);
+    }
+
+    public function subtract(array $A = [], array $B = [])
+    {
+        $A = empty($A) ? $this->A : $A;
+        $B = empty($B) ? $this->B : $B;
+        if (empty($A) || empty($B)) {
+            return 'Error: not specified matrices for the sum operation.';
+        }
+        if (!$this->isMatrix($A) || !$this->isMatrix($B)) {
+            return 'Error: at least one input matrix has wrong shape';
+        }
+        if (!$this->haveEqualDimensions($A, $B)) {
+            return 'Error: matrices have different dimensions.';
+        }
+        $item = function ($aRow, $bRow) use (&$item) {
+            if (!is_array($aRow) && !is_array($bRow)) {
+                return $aRow - $bRow;
+            }
+    
+            return array_map(function ($a, $b) use ($item) {
+                return $item($a, $b);
+            }, $aRow, $bRow);
+        };
+    
+        return $item($A, $B);
     }
 
 }
--- test.php
+++ test.php
@@ -4,5 +4,36 @@ namespace App;
 
 require 'MatrixOperation.php';
 
-$result = new MatrixOperation([1], [2]);
-echo $result->sum([1], [2]);
+$A0 = [];
+
+$A1 = [
+    [1, 2, 3],
+    [1, 1, 1],
+    [0, 0, 0],
+];
+$B1 = [
+    [3, 2, 1],
+    [4, 4, 4],
+    [1, 2, 3],
+];
+$A2 = [1];
+$B2 = [2];
+$A3 = [1, 2, 3];
+$B3 = [3, 2, 1];
+
+$matrix = new MatrixOperation();
+$sum = $matrix->sum($A2, $B2);
+$subtr = $matrix->subtract($A2, $B2);
+printResult($sum);
+
+
+//print function
+function printResult($result) {
+    if (is_array($result[0] ?? null)) {
+        foreach ($result as $row) {
+            echo "[" . implode(" ", $row) . "]\n";
+        }
+    } else {
+        echo "[" . implode(" ", $result) . "]\n";
+    }
+}
